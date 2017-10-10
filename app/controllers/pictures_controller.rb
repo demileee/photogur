f4a90f53ddd53893317f3c597a342655761efd09
Demi Lee <demilee.to@gmail.com>
2017-10-10 13:53:41 -0400

    add new and create routes, new view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,4 +4,16 @@ def index
   @pictures = Picture.all
 end
 
+def new
+  @picture = Picture.new
+end
+
+def create
+  render text: "Received POST request to '/pictures' with the data URL: #{params}"
+end
+
+def show
+  @picture = Picture.find(params[:id])
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pictures' => 'pictures#index'
+  get 'pictures/new' => 'pictures#new'
+  post 'pictures' => 'pictures#create'
+  get 'pictures/:id' => 'pictures#show'
 end
